cde1578002fd01aa789569b128e54db4e4aef211
devseag <ageevvlsergey@gmail.com>
2023-03-04 20:04:25 -0400

    a few changes

---
+++ .ipynb_checkpoints/module-checkpoint.py
@@ -0,0 +1,35 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "939d7e7a-df4c-4913-ae41-d75276ba39b5",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "print(\"Hello\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
